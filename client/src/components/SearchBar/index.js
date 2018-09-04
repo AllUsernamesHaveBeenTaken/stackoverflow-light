ca56705b7f549e41eb4e0e91cd84688fcbaa1da6
Seppe Snoeck <seppe.snoeck@student.odisee.be>
2018-09-04 19:16:33 +0200

    Add some styling

--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -2,10 +2,25 @@ import React from 'react';
 
 import { FilterConsumer } from '../../Contexts/FilterContext';
 
+const boxStyle = {
+  width: '25%',
+  border: 'solid #2E3532 1px',
+  borderRadius: 15,
+  fontSize: 15,
+  padding: '5px 15px',
+  outline: 'none'
+};
+
 const SearchBar = () => (
   <FilterConsumer>
     {({ filter, onChangeFilter }) => (
-      <input type="text" value={filter} onChange={({ target }) => onChangeFilter(target.value)} />
+      <input
+        style={boxStyle}
+        type="text"
+        value={filter}
+        onChange={({ target }) => onChangeFilter(target.value)}
+        placeholder="Search"
+      />
     )}
   </FilterConsumer>
 );
