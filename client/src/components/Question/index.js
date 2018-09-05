242d9bf78db57a7a97e2554d9e5f7ff1b63f5aff
Seppe Snoeck <seppe.snoeck@student.odisee.be>
2018-09-05 17:48:11 +0200

    Add new Route and single question query

--- client/src/components/Question/index.js
+++ client/src/components/Question/index.js
@@ -1,3 +1,4 @@
 export { default as QuestionItem } from './Item';
 export { default as QuestionList } from './List';
 export { default as AskQuestion } from './AskQuestion';
+export { default as QuestionDetail } from './Detail';
--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,7 +1,10 @@
 async function feed(parent, args, context, info) {
-  const where = args.filter ? { title_contains: args.filter } : {}
+  const where = args.filter ? { title_contains: args.filter } : {};
 
-  const queriedQuestions = await context.db.query.questions({ where }, `{ id }`)
+  const queriedQuestions = await context.db.query.questions(
+    { where },
+    `{ id }`
+  );
 
   const countSelectionSet = `
     {
@@ -9,16 +12,24 @@ async function feed(parent, args, context, info) {
         count
       }
     }
-  `
+  `;
 
-  const questionsConnection = await context.db.query.questionsConnection({ where }, countSelectionSet)
+  const questionsConnection = await context.db.query.questionsConnection(
+    { where },
+    countSelectionSet
+  );
 
   return {
     count: questionsConnection.aggregate.count,
     questionIds: queriedQuestions.map(question => question.id)
-  }
+  };
+}
+
+function question(parent, args, context, info) {
+  return context.db.query.question({ where: { id: args.id } }, info);
 }
 
 module.exports = {
-  feed
+  feed,
+  question
 };
